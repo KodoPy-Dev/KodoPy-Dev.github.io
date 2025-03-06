42df6062511698c53ea5e172046ce774584be200
KodoPy <kodopy.dev@gmail.com>
2025-03-06 15:36:25 -0500

    Writing

--- dev/testing.py
+++ dev/testing.py
@@ -1,12 +1,10 @@
-# Convert string to float
-def to_float(value, fallback=None):
-    try: return float(value)
-    except: pass
-    return fallback
 
-a = "10.5416"
-b = to_float(value=a, fallback=None)
-
-print(b)
-
-
+# Nested for loops
+nested_items = [
+    (1, 2, 3),
+    (4, 5, 6),
+    (7, 8, 9),
+]
+for item in nested_items:
+    for number in item:
+        print(number)
